b0edfa03c867c1f6a381e7b0bd138a530e08ab35
chriscauley <chris@lablackey.com>
2016-11-03 15:57:40 -0400

    adding user foreignkey

--- graphene_demo/models.py
+++ graphene_demo/models.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.db import models
 
 class Animal(models.Model):
   name = models.CharField(max_length=32)
   genus = models.CharField(max_length=32)
   is_domesticated = models.BooleanField(default=False)
+  user = models.ForeignKey(settings.AUTH_USER_MODEL,null=True,blank=True)
   __unicode__ = lambda self: self.name
